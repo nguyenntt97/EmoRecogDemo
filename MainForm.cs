3d2d9af5effe1f1fa758b96bec76f54dee377d1f
Tung Nguyên <rohirrim3105@gmail.com>
2018-04-08 20:23:16 +0700

    add camera data stream utils by avi32.dll

---
+++ MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceRecog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public TCamDevice camDevice { set; get; }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            using (FormDevices deviceWd = new FormDevices())
+            {
+                deviceWd.ShowInTaskbar = false;
+                deviceWd.ShowDialog();
+
+                camDevice = deviceWd.selDevice;
+
+                camDevice.ShowWindow(this.pbCamera);
+            }
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ## INTRODUCTION
 So far, this is one of my Toy projects in 2018. The aim of this project is to learn Neural Network and reinforce my C# coding skill.
 Thank for your attention and wish you have as much good time as I had with this demo.
+
+## PREREQUISITE
+*EmguCV v3.4.1.2976 (OpenCV C# Wrapper): https://sourceforge.net/projects/emgucv/?source=typ_redirect
