36b37010a9846f7392edfd512fa38d3cb61f6c4b
Tung Nguyên <rohirrim3105@gmail.com>
2018-04-09 22:04:17 +0700

    modify project tree

---
+++ FaceRecog/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace FaceRecog
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.ibCamera = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ibCamera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(343, 512);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(206, 51);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // ibCamera
+            // 
+            this.ibCamera.Location = new System.Drawing.Point(25, 23);
+            this.ibCamera.Name = "ibCamera";
+            this.ibCamera.Size = new System.Drawing.Size(841, 467);
+            this.ibCamera.TabIndex = 2;
+            this.ibCamera.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 575);
+            this.Controls.Add(this.ibCamera);
+            this.Controls.Add(this.btnStart);
+            this.Name = "MainForm";
+            this.Text = "Emotion Detector Demo";
+            ((System.ComponentModel.ISupportInitialize)(this.ibCamera)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private Emgu.CV.UI.ImageBox ibCamera;
+    }
+}
+
---
+++ FaceRecog/MainForm.cs
@@ -0,0 +1,118 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceRecog
+{
+    public partial class MainForm : Form
+    {
+        private VideoCapture capture;
+        private CascadeClassifier haar = new CascadeClassifier(Environment.CurrentDirectory + "\\..\\..\\haarcascade_frontalface_alt_tree.xml");
+        private const int FPS = 50;
+        private bool isRunning = false;
+        private Timer timer;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            // Console.WriteLine(Path.Combine(Environment.CurrentDirectory, @"..\..\Resources\haarcascade_frontalface_alt_tree.xml"));
+        }
+
+        // public TCamDevice camDevice { set; get; }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            /*
+            using (FormDevices deviceWd = new FormDevices())
+            {
+                deviceWd.ShowInTaskbar = false;
+                deviceWd.ShowDialog();
+
+                camDevice = deviceWd.selDevice;
+
+                camDevice.ShowWindow(this.pbCamera);
+            }
+            */
+
+            /*
+            Thread refreshThread = new Thread(refreshFrame);
+            refreshThread.Start();
+            */
+            if (!isRunning)
+            {
+                capture = new VideoCapture();
+
+                if (timer == null)
+                {
+                    timer = new Timer();
+                    timer.Interval = 1000 / FPS;
+                    timer.Tick += refreshFrame;
+                }
+
+                timer.Start();
+                btnStart.Text = "Pause";
+
+                isRunning = true;
+            } else
+            {
+                timer.Stop();
+                capture.Dispose();
+
+                btnStart.Text = "Start";
+
+                isRunning = false;
+            }
+        }
+
+        private void refreshFrame(object sender, EventArgs e)
+        {
+            // Image<Bgr, byte> temp = null;
+
+            using (Image<Bgr, byte> nextFrame = capture.QueryFrame().ToImage<Bgr, byte>())
+            {
+                if (nextFrame != null)
+                {
+                    // there's only one channel (greyscale), hence the zero index
+                    // var faces = nextFrame.DetectHaarCascade(haar)[0];
+                    ibCamera.Image = nextFrame;
+
+                    var grayframe = nextFrame.Convert<Gray, byte>();
+                    try
+                    {
+                        var faces = haar.DetectMultiScale(grayframe, 1.1, 10, Size.Empty); //the actual face detection happens here
+                        foreach (var face in faces)
+                        {
+                            nextFrame.Draw(face, new Bgr(Color.BurlyWood), 3); //the detected face(s) is highlighted here using a box that is drawn around it/them
+                        }
+
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                }
+
+            }
+
+
+        }
+
+
+    }
+}
+
