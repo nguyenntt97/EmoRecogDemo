0d93ac1fb3e49de9fa1ff4af65a5b3444421209f
Tung Nguyên <rohirrim3105@gmail.com>
2018-04-10 11:36:15 +0700

    add new landmark trained model

--- FaceRecog/MainForm.cs
+++ FaceRecog/MainForm.cs
@@ -1,6 +1,7 @@
 ﻿using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
 using Emgu.Util;
 using System;
 using System.Collections.Generic;
@@ -12,13 +13,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DlibDotNet;
 
 namespace FaceRecog
 {
     public partial class MainForm : Form
     {
         private VideoCapture capture;
-        private CascadeClassifier haar = new CascadeClassifier(Environment.CurrentDirectory + "\\..\\..\\haarcascade_frontalface_alt_tree.xml");
+        private CascadeClassifier faceCsc = new CascadeClassifier(Environment.CurrentDirectory + "\\..\\..\\haarcascade_frontalface_alt_tree.xml");
+
+        FrontalFaceDetector frontalFaceDetector = FrontalFaceDetector.GetFrontalFaceDetector();
+        ShapePredictor predictor = new ShapePredictor(Services.Utils.getResourceURI("Resources/shape_predictor_68_face_landmarks.dat"));
+
         private const int FPS = 50;
         private bool isRunning = false;
         private Timer timer;
@@ -26,6 +32,7 @@ namespace FaceRecog
         public MainForm()
         {
             InitializeComponent();
+
             // Console.WriteLine(Path.Combine(Environment.CurrentDirectory, @"..\..\Resources\haarcascade_frontalface_alt_tree.xml"));
         }
 
@@ -58,14 +65,15 @@ namespace FaceRecog
                 {
                     timer = new Timer();
                     timer.Interval = 1000 / FPS;
-                    timer.Tick += refreshFrame;
+                    timer.Tick += RefreshFrame;
                 }
 
                 timer.Start();
                 btnStart.Text = "Pause";
 
                 isRunning = true;
-            } else
+            }
+            else
             {
                 timer.Stop();
                 capture.Dispose();
@@ -76,42 +84,72 @@ namespace FaceRecog
             }
         }
 
-        private void refreshFrame(object sender, EventArgs e)
+        private void RefreshFrame(object sender, EventArgs e)
         {
             // Image<Bgr, byte> temp = null;
 
-            using (Image<Bgr, byte> nextFrame = capture.QueryFrame().ToImage<Bgr, byte>())
+            //using (Image<Bgr, byte> nextFrame = capture.QueryFrame().ToImage<Bgr, byte>())
+            //{
+            //    if (nextFrame != null)
+            //    {
+            //        // there's only one channel (greyscale), hence the zero index
+            //        // var faces = nextFrame.DetectHaarCascade(haar)[0];
+            //        ibCamera.Image = nextFrame;
+
+            //        var grayframe = nextFrame.Convert<Gray, byte>();
+            //        try
+            //        {
+            //            var faces = faceCsc.DetectMultiScale(grayframe, 1.1, 10, Size.Empty); //the actual face detection happens here
+            //            foreach (var face in faces)
+            //            {
+            //                nextFrame.Draw(face, new Bgr(Color.BurlyWood), 3); //the detected face(s) is highlighted here using a box that is drawn around it/them
+            //            }
+
+
+
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            Console.WriteLine(ex.Message);
+            //        }
+
+            //    }
+
+            //}
+
+            using (Image<Bgr, byte> curFrame = capture.QueryFrame().ToImage<Bgr, byte>())
             {
-                if (nextFrame != null)
-                {
-                    // there's only one channel (greyscale), hence the zero index
-                    // var faces = nextFrame.DetectHaarCascade(haar)[0];
-                    ibCamera.Image = nextFrame;
+                Mat gray = null;
+                // Convert multi-channel BGR image to gray
+                CvInvoke.CvtColor(curFrame, gray, ColorConversion.Bgr2Gray);
 
-                    var grayframe = nextFrame.Convert<Gray, byte>();
-                    try
-                    {
-                        var faces = haar.DetectMultiScale(grayframe, 1.1, 10, Size.Empty); //the actual face detection happens here
-                        foreach (var face in faces)
-                        {
-                            nextFrame.Draw(face, new Bgr(Color.BurlyWood), 3); //the detected face(s) is highlighted here using a box that is drawn around it/them
-                        }
+                CvInvoke.EqualizeHist(gray, gray);
 
-
-
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-
-                }
-
+                var detects = frontalFaceDetector.Detect(gray);
             }
-
-
+            
         }
 
+        // ROI: Region of Interest
+        private bool myDetector(InputArray image, out System.Drawing.Rectangle[] ROIs)
+        {
+            Mat gray = null;
+
+            // Convert multi-channel BGR image to gray
+            if (image.GetChannels() > 1)
+            {
+                CvInvoke.CvtColor(image.GetMat(), gray, ColorConversion.Bgr2Gray);
+            }
+            else
+            {
+                gray = image.GetMat().Clone();
+            }
+
+            CvInvoke.EqualizeHist(gray, gray);
+            ROIs = faceCsc.DetectMultiScale(gray, 1.1, 3, Size.Empty);
+            return true;
+        }
+        // public abstract void GetLandmarks();
 
     }
 }
